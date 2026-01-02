349a43d31c00451fb410bc9e551f57fb322918a4
jiruneko <oohikaru310@gmail.com>
2026-01-03 01:56:00 +0900

    Add new feature

--- ch03-1/main.go
+++ ch03-1/main.go
@@ -5,8 +5,8 @@ import (
 )
 
 func main() {
-	var x = [3]int{10, 20, 30}
-	var y = [12]int{1, 5: 4, 6, 10: 100, 15}
+	var x = [4]int{10, 20, 30}
+	var y = [12]int{1, 1: 4, 5: 100, 15}
 	fmt.Println(x, y)
 	s := []string{"a", "b", "c"}
 	fmt.Println(s)
--- ch03-2/main.go
+++ ch03-2/main.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var x = [...]int{1, 2, 3}
+	var y = [3]int{1, 2, 3}
+	fmt.Println(x == y)
+	x[0] = 10
+	fmt.Println(x[0])
+}
